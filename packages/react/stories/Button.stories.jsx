1619e4b8dc9a4864a8ba4b607e4ca52b22715d08
Yatharth K <yatharth@hey.com>
2022-07-02 10:24:12 +0530

    storybook setup

---
+++ packages/react/stories/Button.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from '../components/button';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Button',
+  component: Button,
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Button {...args}>Button</Button>;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
+  children: 'Button',
+};
