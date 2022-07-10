e372ea95f80d2b3498da5a7f68a2d69124a96f10
Yatharth K <yatharth01@gmail.com>
2022-07-10 11:18:44 +0530

    TextField

---
+++ packages/react/stories/TextField.stories.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { TextField } from '../components/TextField'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  argTypes: {
+    size: {
+      options: ['1', '2'],
+      control: {type: 'select'}
+    },
+    variant: {
+      options: ['ghost'],
+      control: {type: 'select'}
+    },
+    state: {
+      options: ['invalid', 'valid'],
+      control: {type: 'select'}
+    },
+    cursor: {
+      options: ['default', 'text'],
+      control: {type: 'select'}
+    },
+    as: {
+      control: { disable: true }
+    },
+    css: {
+      control: { disable: true }
+    }
+  }
+};
+
+const Template = ({text, ...args}) => <TextField {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  placeholder: 'Your email',
+  size: '1',
+};
