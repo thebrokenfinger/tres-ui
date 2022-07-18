286f20c3bce55dcc4dabfb1f417714bd55a17cc8
Yatharth K <yatharth01@gmail.com>
2022-07-18 10:23:05 +0530

    Component exports for lib use

--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,3 +1,24 @@
-import { Button } from "./components/Button";
-
-export { Button };
+export { Anchor } from "./components/Anchor";
+export { Box } from "./components/Box";
+export { Button } from "./components/Button";
+export { Code } from "./components/Code";
+export { ControlGroup } from "./components/ControlGroup";
+export { Flex } from "./components/Flex";
+export { Grid } from "./components/Grid";
+export { Heading } from "./components/Heading";
+export { IconButton } from "./components/IconButton";
+export { Panel } from "./components/Panel";
+export { Paragraph } from "./components/Paragraph";
+export {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "./components/Popover";
+export { ProgressBar } from "./components/ProgressBar";
+export { Radio, RadioGroup } from "./components/Radio";
+export { RadioCard, RadioCardGroup } from "./components/RadioCard";
+export { Select } from "./components/Select";
+export { Text } from "./components/Text";
+export { TextArea } from "./components/TextArea";
+export { TextField } from "./components/TextField";
