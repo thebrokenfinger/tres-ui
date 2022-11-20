4b7049e19c5419f21eee715a9cadcec8fd9ee761
Yatharth K <yatharth@hey.com>
2022-11-20 12:19:47 +0530

    chore: pnpm and turborepo setup

---
+++ apps/examples/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@tres-ui/react";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>TresUI</h1>
+      <Button>I&apos;m a Button</Button>
+    </div>
+  );
+}
